6ddf27a62e347368960ef101001600f07fb9cbbc
rene <renegmed@yahoo.com>
2018-07-25 08:15:30 -0400

    2. created reusable utilities

--- source/bootstrap/button.js
+++ source/bootstrap/button.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import {classify} from './utils';
 
 export default class Button extends React.Component {
-    classify(key,value) {
-        if (value[0]==='-') value = key+value;
-        value =  value.split(' -').join(` ${key}-`);       
-        return `${key} ${value}`;
-    }
+     
     render() {
         const Tag = this.props.href? 'a' : 'button';
         //const className = 'btn ' + this.props.className;
         //this.props.className = className;
-        const className = this.classify('btn', this.props.className) + 
+        const className = classify('btn', this.props.className) + 
             (this.props.disabled?' disabled':'');
 
         return (
