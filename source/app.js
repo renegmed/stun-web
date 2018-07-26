ab932341cc310eb01209e5b659b939e64d50cf25
rene <renegmed@yahoo.com>
2018-07-26 16:35:17 -0400

    14.animating children with ReactCSSTransitionGroup

--- source/app.js
+++ source/app.js
@@ -10,15 +10,23 @@ export default class App extends Component {
 
         // binding tells this.onClick to call onClick(e)
         this.onClick = this.onClick.bind(this);
-        this.state = {click: 0};
+        this.state = {click: 0, labels: []};
     }
     onClick(e){
-        this.setState({click: this.state.click+1 });      
-        //console.log(this.state.click);
+        let labels = this.state.labels.concat([this.state.click]);      
+        this.setState({click:this.state.click+1,labels:labels });        
     }
     render() {
+
+        console.log('----- render() -----');
+        let buttons = this.state.labels.map((value, i) => {           
+            return <Button key={i}>{value}</Button>
+        });
+ 
         const useContainerFluid = true;
-        return <ReactCSSTransitionGroup transitionName='app' transitionAppear={true} transitionAppearTimeout={1000}> 
+        return <ReactCSSTransitionGroup transitionName='app' 
+             transitionAppear={true} transitionAppearTimeout={1000}
+                transitionEnterTimeout={500} transitionLeaveTimeout={500}> 
         <Jumbotron className="-fluid custom"            
            containerFluid={useContainerFluid}>
 
@@ -26,9 +34,12 @@ export default class App extends Component {
        <p>with Bootstrap and SaSS.</p>
        <p><Button className="-primary " href="http://02geek.com/" target="_blank" label="Discover things"/></p>
        <p><Button className="-danger-outline -sm">These are good stuffs</Button></p>
-       <p><Button onClick={this.onClick} className="-primary -lg -block" 
-                label={this.state.click==0? '0': this.state.click}/></p>
+       <p><Button onClick={this.onClick} className="-primary -lg -block">{this.state.click}</Button></p>
      
+       <ReactCSSTransitionGroup transitionName='app'              
+                transitionEnterTimeout={500} transitionLeaveTimeout={500}> 
+            {buttons}
+        </ReactCSSTransitionGroup>
    </Jumbotron>
    </ReactCSSTransitionGroup>
     }
--- source/bootstrap/utils.js
+++ source/bootstrap/utils.js
@@ -1,4 +1,5 @@
 export function classify(key,value) {
+    if (typeof value == 'undefined' || value == null) return;
     if (value[0]==='-') value = key+value;
     value =  value.split(' -').join(` ${key}-`);       
     return `${key} ${value}`;
