f38f0687a6e47c80a9ad219436740fe64452fab8
rene <renegmed@yahoo.com>
2018-07-26 11:13:24 -0400

    13. understanding how state works

--- source/app.js
+++ source/app.js
@@ -10,28 +10,24 @@ export default class App extends Component {
 
         // binding tells this.onClick to call onClick(e)
         this.onClick = this.onClick.bind(this);
+        this.state = {click: 0};
     }
     onClick(e){
-        console.log('---------');
-        console.log(this);
+        this.setState({click: this.state.click+1 });      
+        //console.log(this.state.click);
     }
     render() {
         const useContainerFluid = true;
         return <ReactCSSTransitionGroup transitionName='app' transitionAppear={true} transitionAppearTimeout={1000}> 
         <Jumbotron className="-fluid custom"            
            containerFluid={useContainerFluid}>
-        
-            {/* 
-            if containerFluid == true will result to 
-                <div class="container-fluid" ... 
-            otherwise 
-                <div class="container" ...     */}
-         
+
        <h1>Building React.js User Interfaces</h1>
        <p>with Bootstrap and SaSS.</p>
        <p><Button className="-primary " href="http://02geek.com/" target="_blank" label="Discover things"/></p>
        <p><Button className="-danger-outline -sm">These are good stuffs</Button></p>
-       <p><Button onClick={this.onClick} className="-primary -lg -block" label="Discover things and stuff"  /></p>
+       <p><Button onClick={this.onClick} className="-primary -lg -block" 
+                label={this.state.click==0? '0': this.state.click}/></p>
      
    </Jumbotron>
    </ReactCSSTransitionGroup>
