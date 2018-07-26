711529dff821cb2a64c0a34135a9a798f1073ef9
rene <renegmed@yahoo.com>
2018-07-26 17:04:15 -0400

    15.leaving animation with sass nesting

--- source/app.js
+++ source/app.js
@@ -10,17 +10,22 @@ export default class App extends Component {
 
         // binding tells this.onClick to call onClick(e)
         this.onClick = this.onClick.bind(this);
+        this.onReset = this.onReset.bind(this);
         this.state = {click: 0, labels: []};
     }
     onClick(e){
         let labels = this.state.labels.concat([this.state.click]);      
         this.setState({click:this.state.click+1,labels:labels });        
     }
+    onReset(e){          
+        this.setState({click: 0,labels: [] });        
+    }
+
     render() {
 
         console.log('----- render() -----');
         let buttons = this.state.labels.map((value, i) => {           
-            return <Button key={i}>{value}</Button>
+            return <Button key={value}>{value}</Button>
         });
  
         const useContainerFluid = true;
@@ -33,11 +38,13 @@ export default class App extends Component {
        <h1>Building React.js User Interfaces</h1>
        <p>with Bootstrap and SaSS.</p>
        <p><Button className="-primary " href="http://02geek.com/" target="_blank" label="Discover things"/></p>
-       <p><Button className="-danger-outline -sm">These are good stuffs</Button></p>
+       <p><Button onClick={this.onReset}className="-danger-outline -sm">Reset</Button></p>
        <p><Button onClick={this.onClick} className="-primary -lg -block">{this.state.click}</Button></p>
      
-       <ReactCSSTransitionGroup transitionName='app'              
-                transitionEnterTimeout={500} transitionLeaveTimeout={500}> 
+       <ReactCSSTransitionGroup 
+                transitionName='app'              
+                transitionEnterTimeout={3000} 
+                transitionLeaveTimeout={2000}> 
             {buttons}
         </ReactCSSTransitionGroup>
    </Jumbotron>
