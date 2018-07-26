597b05b7672c050216736637cfda8867df9a0e7f
rene <renegmed@yahoo.com>
2018-07-26 11:00:13 -0400

    12. working with events in react

--- source/app.js
+++ source/app.js
@@ -5,6 +5,16 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        // binding tells this.onClick to call onClick(e)
+        this.onClick = this.onClick.bind(this);
+    }
+    onClick(e){
+        console.log('---------');
+        console.log(this);
+    }
     render() {
         const useContainerFluid = true;
         return <ReactCSSTransitionGroup transitionName='app' transitionAppear={true} transitionAppearTimeout={1000}> 
@@ -19,9 +29,9 @@ export default class App extends Component {
          
        <h1>Building React.js User Interfaces</h1>
        <p>with Bootstrap and SaSS.</p>
-       <p><Button className="-primary" href="http://02geek.com/" target="_blank" label="Discover things"/></p>
+       <p><Button className="-primary " href="http://02geek.com/" target="_blank" label="Discover things"/></p>
        <p><Button className="-danger-outline -sm">These are good stuffs</Button></p>
-       <p><Button className="-primary -lg -block" label="Discover things and stuff"  /></p>
+       <p><Button onClick={this.onClick} className="-primary -lg -block" label="Discover things and stuff"  /></p>
      
    </Jumbotron>
    </ReactCSSTransitionGroup>
